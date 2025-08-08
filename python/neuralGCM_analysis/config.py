0cdd63a6ee7ad4a9217047198016ca55ab80feb9
Spencer Ressel <sressel@crhtc41.hpc.ucar.edu>
2025-08-07 18:29:51 -0600

    regular updates

---
+++ python/neuralGCM_analysis/config.py
@@ -0,0 +1,6 @@
+import sys
+sys.path.insert(0, "/glade/u/home/sressel/thesis-work/python/auxiliary_functions/")
+
+str_width = 40
+data_directory = f"/glade/u/home/sressel/thesis-work/python/neuralGCM_analysis/data"
+neuralGCM_output_directory = f"/glade/u/home/sressel/thesis-work/python/neuralGCM_analysis/output/"
