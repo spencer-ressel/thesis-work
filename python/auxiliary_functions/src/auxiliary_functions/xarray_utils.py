66a36299faf60ed15e1516789de2c5a5eb0e91e4
Spencer Ressel <ressel.spencer@gmail.com>
2026-07-20 17:45:44 -0600

    EOD 07/20/2026 - Analysis to try and understand zonal structure of mean meridional winds

--- python/auxiliary_functions/src/auxiliary_functions/xarray_utils.py
+++ python/auxiliary_functions/src/auxiliary_functions/xarray_utils.py
@@ -298,6 +298,45 @@ class _CoordFuncsMixin:
 
         return new_data
 
+    def add_cyclic_point(self, dim='lon'):
+        """
+        Add a cyclic (wrap-around) point to an xarray.DataArray along a given dimension.
+
+        Parameters
+        ----------
+        dataarray : xarray.DataArray
+            The input DataArray. Must include the coordinate specified by `dim`.
+            Typically has longitude as one of the dimensions.
+        dim : str, optional
+            The name of the dimension along which to add the cyclic point.
+            Defaults to 'longitude'.
+
+        Returns
+        -------
+        xarray.DataArray
+            A new DataArray with an extra grid point appended to close the cyclic boundary.
+            The output preserves the original coordinates, dimensions, attributes,
+            and name of the input DataArray.
+        """
+
+        # Add cyclic point and get updated coordinate
+        data = self._obj
+
+        new_data, new_coord = util.add_cyclic_point(data.values, coord=data[dim].values, axis=data.get_axis_num(dim))
+
+        # Copy coordinates and update the chosen one
+        new_coords = dict(data.coords)
+        new_coords[dim] = new_coord
+
+        # Rebuild the DataArray with updated longitude
+        return xr.DataArray(
+            new_data,
+            dims=data.dims,
+            coords=new_coords,
+            attrs=data.attrs,
+            name=data.name
+        )
+
     def sel(self, *args, **kwargs):
         """
         Safe selector that only applies selections for coordinates/dimensions
