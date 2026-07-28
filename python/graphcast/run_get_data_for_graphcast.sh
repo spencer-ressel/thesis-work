ca0da5f6996ccd6d83f761c5b29293eeadef4a9f
Spencer Ressel <ressel.spencer@gmail.com>
2026-07-28 17:58:43 -0600

    EOD 07/28/2026 - More data downloads, preliminary processing of climatology data

--- python/graphcast/run_get_data_for_graphcast.sh
+++ python/graphcast/run_get_data_for_graphcast.sh
@@ -13,8 +13,8 @@ CONDA_ENV="modified-npl"
 module load conda 2>/dev/null || true
 conda activate "${CONDA_ENV}"
 
-START_DATE="2001-06-01T00:00:00.000000000"
-END_DATE="2001-12-31T00:00:00.000000000"
+START_DATE="2000-06-01T00:00:00.000000000"
+END_DATE="2000-12-31T00:00:00.000000000"
 
 start_time=$(date +%s)
 
