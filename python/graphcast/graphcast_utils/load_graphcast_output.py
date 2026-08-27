977f327fab9622bcb3caf94625ada91569f33d66
Spencer Ressel <ressel.spencer@gmail.com>
2026-08-27 17:07:08 -0600

    EOD 08/27/2026 - Ran more analysis, calculated composite loss relative to ERA5 for Control and Perfect for CWV optimizations

--- python/graphcast/graphcast_utils/load_graphcast_output.py
+++ python/graphcast/graphcast_utils/load_graphcast_output.py
@@ -9,10 +9,11 @@ def load_predictions(prediction_initial_date, prediction_timesteps, prediction_t
         f"{config.GRAPHCAST_DATA_DIRECTORY}/{prediction_type}_model_forecasts/{prediction_initial_date}/{prediction_initial_date}_{prediction_timesteps}{run_notes}.zarr"
     )
 
-    predictions = xr.open_zarr(prediction_filepath, drop_variables=vars_to_drop).load().sel(batch=0, drop=True)
+    # predictions = xr.open_zarr(prediction_filepath, drop_variables=vars_to_drop).load().sel(batch=0, drop=True)
+    predictions = xr.open_zarr(prediction_filepath, drop_variables=vars_to_drop).sel(batch=0, drop=True)
 
     if decode_time:
         datetimes = convert_ns_to_datetime(predictions.time, prediction_initial_date)
-        predictions = predictions.assign_coords({'time':datetimes.datetime}).drop_vars(["datetime"])
+        predictions = predictions.assign_coords({'time':datetimes.datetime}).drop_vars(["datetime", "initial_conditions_path"])
 
     return predictions
